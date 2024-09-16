2e0ca8cb255470b8c6ba948ba28a513cc12972a6
usmanbaig3699 <79605870+usmanbaig3699@users.noreply.github.com>
2024-09-16 21:47:41 +0500

    email send from gmail

--- src/config/nodeMailer.js
+++ src/config/nodeMailer.js
@@ -1,19 +1,48 @@
 const nodemailer = require('nodemailer');
-const sendGridTransport = require('nodemailer-sendgrid-transport');
+//const sendGridTransport = require('nodemailer-sendgrid-transport');
 const {
   generateNewPhraseEmail,
   generateOTPEmail,
 } = require('../utils/commonUtils/generateEmail');
+const { google } = require('googleapis');
 
-const transporter = nodemailer.createTransport(
-  sendGridTransport({
-    auth: {
-      api_key: process.env.SENDGRID_API_KEY,
-    },
-  })
+
+// OAuth2 setup
+const OAuth2 = google.auth.OAuth2;
+const oauth2Client = new OAuth2(
+  process.env.MAIL_CLIENT_ID,
+  process.env.MAIL_CLIENT_SECRET_ID,
+  'https://developers.google.com/oauthplayground' // Redirect URL
 );
 
+// Set the refresh token
+oauth2Client.setCredentials({
+  refresh_token: process.env.MAIL_REFRESH_TOKEN,// Your refresh token
+});
+// const transporter = nodemailer.createTransport(
+//   sendGridTransport({
+//     auth: {
+//       api_key: process.env.SENDGRID_API_KEY,
+//     },
+//   })
+// );
+
 async function sendSignUpMail(email, otp) {
+
+  const accessToken = await oauth2Client.getAccessToken();
+
+const transporter = nodemailer.createTransport({
+  service: process.env.MAIL_SERVICE,
+  auth: {
+    type: process.env.MAIL_AUTH_TYPE,
+    user: process.env.MAIL_AUTH_USER,
+    clientId: process.env.MAIL_CLIENT_ID,
+    clientSecret: process.env.MAIL_CLIENT_SECRET_ID,
+    refreshToken: process.env.MAIL_REFRESH_TOKEN,
+    accessToken: accessToken.token,
+  },
+});
+
   const data = {
     heading: 'confirm your email address',
     message: `your confirmation code is below - enter it in and we'll help you get registered.`,
